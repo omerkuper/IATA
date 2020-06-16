74d2ffa30d8e4ee36c095b769f885507959661ac
omerkuper <40544034+omerkuper@users.noreply.github.com>
2020-06-16 16:32:19 +0300

    Update tst.py

--- tst.py
+++ tst.py
@@ -4,9 +4,9 @@ from datetime import date, timedelta
 YesNo = {'y': 'true', 'n': 'false'}
 
 start = ['LHR', 'ams', 'nyc']
-end = ['EZE', 'ttt', 'aaa']
-date_i = [200901, 230401, 220301]
-stay_in = [3]
+end = ['EZE', 'ttt']
+date_i = [200901, 220101]
+stay_in = [3,7,9]
 loop = 2
 direct_flight = 'y'
 
@@ -21,68 +21,7 @@ def RoundTrip():
 
                 if len(date_i) == 1:
                     x = 0
-                elif len(date_i) == len(start) and len(start) != len(end):
-                    x = counter_out
-                elif len(date_i) == len(end) and len(start) != len(end):
-                    x = destination
-                elif len(start) == len(end):
-                    x = destination
-                else:
-                    x = destination
-               
-                
-                if len(stay_in) == 1:
-                    y = 0
-                elif len(stay_in) == len(start) and len(start) != len(end):
-                    y = counter_out
-                elif len(stay_in) == len(end) and len(start) != len(end):
-                    y = destination
-                elif len(start) == len(end):
-                    y = destination
-                else:
-                    y = destination
-
-
-                date_return = time.strptime(str(date_i[x]), '%y%m%d')
-                date_return_i = date(date_return.tm_year, date_return.tm_mon,
-                                     date_return.tm_mday) + timedelta(counter)
-
-                date_return_q = date(date_return.tm_year, date_return.tm_mon,
-                                     date_return.tm_mday) + timedelta(counter + stay_in[y])
-                print(departure, end[destination], date_return_i, date_return_q)
-                counter += 1
-        counter_out += 1
-    return lst
-
-for u in RoundTrip():
-    print(u)  
-
-#################################################################################################################  
-  
-import time
-from datetime import date, timedelta
-
-YesNo = {'y': 'true', 'n': 'false'}
-
-start = ['LHR', 'ams', 'nyc']
-end = ['EZE', 'ttt', 'aaa']
-date_i = [200901, 230401, 220301]
-stay_in = [3]
-loop = 2
-direct_flight = 'y'
-
-
-def RoundTrip():
-    lst = []
-    counter_out = 0
-    for departure in start:
-        for destination in range(len(end)):
-            counter = 0
-            for _ in range(loop):
-
-                if len(date_i) == 1:
-                    x = 0
-                elif len(start) >= len(end):
+                elif len(date_i) == len(start):
                     x = counter_out
                 else:
                     x = destination
@@ -90,12 +29,8 @@ def RoundTrip():
                 
                 if len(stay_in) == 1:
                     y = 0
-                elif len(stay_in) == len(start) and len(start) != len(end):
+                elif len(stay_in) == len(start):
                     y = counter_out
-                elif len(stay_in) == len(end) and len(start) != len(end):
-                    y = destination
-                elif len(start) == len(end):
-                    y = destination
                 else:
                     y = destination
 
@@ -113,6 +48,3 @@ def RoundTrip():
 
 for u in RoundTrip():
     print(u)
-    
-    
- 
