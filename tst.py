98afdc25af3ab08b9752414dbf696b480360c545
omerkuper <40544034+omerkuper@users.noreply.github.com>
2020-06-17 14:53:18 +0300

    Update tst.py

--- tst.py
+++ tst.py
@@ -5,7 +5,7 @@ YesNo = {'y': 'true', 'n': 'false'}
 
 start = ['LHR', 'akl', 'NYC', 'AMS']
 end = ['akl', 'NYC', 'AMS', 'LHR']
-date_i = [200901, 220901]
+date_i = [200901]
 stay_in = [19, 7, 10]
 loop = 3
 direct_flight = 'y'
@@ -32,19 +32,19 @@ def look(counter_out, destination):
 # def urls(route, departure=None, destination=None, date_return_i=None, date_return_q=None, stay_long=None):
 def urls(*args):
     route = args[0]
-    if route is 'OneWay':
+    if route == 'OneWay':
         departure, destination, date_return_i = args[1], args[2], args[3]
         url_sky = f'https://www.skyscanner.co.il/transport/flights/{departure}/{end[destination]}/{date_return_i}/?adultsv2=1&cabinclass=economy&childrenv2=&inboundaltsenabled=false&outboundaltsenabled=false&preferdirects={YesNo[direct_flight]}&rtn=0&priceSourceId=&priceTrace=&qp_prevCurrency=USD&qp_prevPrice=OneWayNone&qp_prevProvider=ins_month'
         url_jet = f'https://search.jetradar.com/flights?marker=google&origin_iata={departure.upper()}&destination_iata={end[destination].upper()}&depart_date={date_return_i}&with_request=true&adults=1&children=0&infants=0&trip_class=0&locale=en&one_way=false&ct_guests=1+passenger&ct_rooms=1'
         url_mom = f'https://www.momondo.com/flight-search/{departure}-{end[destination]}/{date_return_i}?sort=bestflight_a'
 
-    elif route is 'RoundTrip':
+    elif route == 'RoundTrip':
         departure, destination, date_return_i, date_return_q = args[1], args[2], args[3], args[4]
         url_sky = f'https://www.skyscanner.net/transport/flights/{departure}/{end[destination]}/{date_return_i}/{date_return_q}?flexible_origin=true&flexible_depart=direct&flexible_return=direct&adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&rtn=1&preferdirects={YesNo[direct_flight]}&outboundaltsenabled=false&inboundaltsenabled=false&ref=home&currency=USD#results'
         url_jet = f'https://search.jetradar.com/flights?marker=google&origin_iata={departure.upper()}&destination_iata={end[destination].upper()}&depart_date={date_return_i}&return_date={date_return_q}&with_request=true&adults=1&children=0&infants=0&trip_class=0&locale=en&one_way=false&ct_guests=1+passenger&ct_rooms=1'
         url_mom = f'https://www.momondo.com/flight-search/{departure}-{end[destination]}/{date_return_i}/{date_return_q}?sort=bestflight_a'
 
-    elif route is 'MultiCity':
+    elif route == 'MultiCity':
         stay_long, date_return_i = args[1], args[2]
         url_sky = f'https://www.skyscanner.net/transport/flights/{start[stay_long]}/{end[stay_long]}/{date_return_i}?flexible_origin=true&flexible_depart=direct&flexible_return=direct&adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&rtn=0&preferdirects={YesNo[direct_flight]}&outboundaltsenabled=false&inboundaltsenabled=false&ref=home&currency=USD#results'
         url_jet = f'https://search.jetradar.com/flights?marker=google&origin_iata={start[stay_long].upper()}&destination_iata={end[stay_long].upper()}&depart_date={date_return_i}&with_request=true&adults=1&children=0&infants=0&trip_class=0&locale=en&one_way=false&ct_guests=1+passenger&ct_rooms=1'
@@ -110,8 +110,8 @@ def MultiCity():
     return lst
 
 
-for u in MultiCity():
-    print(u)
-
-# for u in OneWay():
+# for u in MultiCity():
 #     print(u)
+
+for u in OneWay():
+    print(u)
